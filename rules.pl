c9482e251c19853e4ee87437aff8e00cf22e6726
Gilyerm <gilyerm@gmail.com>
2019-08-06 17:22:39 +0300

    rules_updated
    includes 125 threads (5x5x5) for inital-guess

--- rules.pl
+++ rules.pl
@@ -98,7 +98,7 @@ extendGoodSeq([L1,L2,L3,L4,L5|R],Cprevs,Count) :-
     NextCount is Count-1,
     extendGoodSeq([L2,L3,L4,L5|R], [C|Cprevs], NextCount).    
 
-extendGoodSeq(_,_,4).
+extendGoodSeq([_,_,_,_],_,4).
 
 testExGoodSeq([L1,L2,L3,L4|R],Size) :-
     Size >= 5,
@@ -141,3 +141,200 @@ runMain2(Seq,Size) :-
         ],
         []
     ).
+% 4,1,2,8,16,32,4,1,2,8,64,16,4,1,2,128,8,16,4,1,256,2,8,16,4,512,1,2,8,32,64,4,1,2,128,32,8,4,1,256,64,2,8,4,128,256,1,2,8,512,16,32,1,2,64,128,8,16,1,32,64,4,128,2,16,32,1,256,4,2,512,8,32,64,1,128,16,2,256,4,32,8,16,64,2,256,1,32,8,128,512,2,1,32,256,128,4,2,64,512,1,8,4,128,64,16,1,256,4,128,64,8,32,2,256,16,64,4,8,512,2,128,16,32,8,4,512,1,256,16,8,32,64,128,256,1,2,512,16,64,4,32,2,256,512,8,16,4,128,256,32,8,16,512,64,1,4,32,256,512,8,1,64,128,256,512,2,8,64,128,256,16,2,32,512,4,1,16,128,32
+
+runMain3(Seq,Size,Start) :-
+    prefixOf(Seq,Start),
+    testExGoodSeq(Seq,Size).
+
+% prefixOf([_|_],[]).
+% prefixOf([X|R1],[X|R2]) :-
+%     prefixOf(R1,R2).
+
+runMain3H(St,Sq,Sz) :-
+    runMain3(Sq,Sz,St).
+
+prefixOf([X|_],[X]).
+prefixOf([X|R1],[X|R2]) :-
+    prefixOf(R1,R2).
+
+ourGuess(
+    [4,1,2,8,16,32,4,1,2,8,64,16,4,1,2,128,8,16,4,1,256,2,8,16,4,512,1,2,8,32,64,4,1,2,128,32,8,4,1,256,64,2,8,4,128,256,1,2,8,512,16,32,1,2,64,128,8,16,1,32,64,4,128,2,16,32,1,256,4,2,512,8,32,64,1,128,16,2,256,4,32,8,16,64,2,256,1,32,8,128,512,2,1,32,256,128,4,2,64,512,1,8,4,128,64,16,1,256,4,128,64,8,32,2,256,16,64,4,8,512,2,128,16,32,8,4,512,1,256,16,8,32,64,128,256,1,2,512,16,64,4,32,2,256,512,8,16,4,128,256,32,8,16,512,64,1,4,32,256,512,8,1,64,128,256,512,2,8,64,128,256,16,2,32,512,4,1,16,128,32]
+).
+
+% concatTo(Target,Src1,Src2).  % Target = Src1+Src2.
+concatTo([],[],[]).            % Stop.
+concatTo([X|R],[X|R_S1],S2) :- % S1 not empty
+    concatTo(R,R_S1,S2).
+concatTo([X|R],[],[X|R_S2]) :- % S1 empty, S2 not empty
+    concatTo(R,[],R_S2).
+
+multiExGoodSeq_N1(Seq,Size,Extra) :-
+    ourGuess(Guess),
+    %Start|_ is Seq,
+    concatTo(Start,Guess,Extra),
+    length(Seq,Size),
+    concatTo(Seq,Start,_),
+    testExGoodSeq(Seq,Size).
+
+runMain_N1(Seq,Size) :-
+    first_solution(Seq, [
+            multiExGoodSeq_N1(Seq,Size, [   2,   4,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2,   4,   8 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2,   4,  64 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2,   4, 256 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2,   4, 512 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2,   8,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2,   8,   4 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2,   8,  64 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2,   8, 256 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2,   8, 512 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2,  64,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2,  64,   4 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2,  64,   8 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2,  64, 256 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2,  64, 512 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2, 256,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2, 256,   4 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2, 256,   8 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2, 256,  64 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2, 256, 512 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2, 512,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2, 512,   4 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2, 512,   8 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2, 512,  64 ]),
+            multiExGoodSeq_N1(Seq,Size, [   2, 512, 256 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8,   2,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8,   2,   4 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8,   2,  64 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8,   2, 256 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8,   2, 512 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8,   4,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8,   4,   2 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8,   4,  64 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8,   4, 256 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8,   4, 512 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8,  64,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8,  64,   2 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8,  64,   4 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8,  64, 256 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8,  64, 512 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8, 256,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8, 256,   2 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8, 256,   4 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8, 256,  64 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8, 256, 512 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8, 512,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8, 512,   2 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8, 512,   4 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8, 512,  64 ]),
+            multiExGoodSeq_N1(Seq,Size, [   8, 512, 256 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64,   2,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64,   2,   4 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64,   2,   8 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64,   2, 256 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64,   2, 512 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64,   4,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64,   4,   2 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64,   4,   8 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64,   4, 256 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64,   4, 512 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64,   8,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64,   8,   2 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64,   8,   4 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64,   8, 256 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64,   8, 512 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64, 256,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64, 256,   2 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64, 256,   4 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64, 256,   8 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64, 256, 512 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64, 512,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64, 512,   2 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64, 512,   4 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64, 512,   8 ]),
+            multiExGoodSeq_N1(Seq,Size, [  64, 512, 256 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256,   2,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256,   2,   4 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256,   2,   8 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256,   2,  64 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256,   2, 512 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256,   4,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256,   4,   2 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256,   4,   8 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256,   4,  64 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256,   4, 512 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256,   8,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256,   8,   2 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256,   8,   4 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256,   8,  64 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256,   8, 512 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256,  64,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256,  64,   2 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256,  64,   4 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256,  64,   8 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256,  64, 512 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256, 512,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256, 512,   2 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256, 512,   4 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256, 512,   8 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 256, 512,  64 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512,   2,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512,   2,   4 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512,   2,   8 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512,   2,  64 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512,   2, 256 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512,   4,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512,   4,   2 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512,   4,   8 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512,   4,  64 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512,   4, 256 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512,   8,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512,   8,   2 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512,   8,   4 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512,   8,  64 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512,   8, 256 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512,  64,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512,  64,   2 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512,  64,   4 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512,  64,   8 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512,  64, 256 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512, 256,   1 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512, 256,   2 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512, 256,   4 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512, 256,   8 ]),
+            multiExGoodSeq_N1(Seq,Size, [ 512, 256,  64 ])
+        ],
+        [
+            on_fail(continue)
+        ]
+    ).
+
+sorted([]).
+sorted([_]).
+sorted([X,Y|R]) :-
+    X < Y,
+    sorted([Y|R]).
+
+nextPossible1([L1,L2,L3,L4,L5], N) :-
+    powOf2(N),
+    not(contains(N,[L1,L2,L3,L4,L5])).
+nextPossible5([L1,L2,L3,L4,L5], [N1,N2,N3,N4,N5]) :-
+    nextPossible1([L1,L2,L3,L4,L5], N1),
+    nextPossible1([L1,L2,L3,L4,L5], N2),
+    nextPossible1([L1,L2,L3,L4,L5], N3),
+    nextPossible1([L1,L2,L3,L4,L5], N4),
+    nextPossible1([L1,L2,L3,L4,L5], N5),
+    noDups([N1,N2,N3,N4,N5]),
+    sorted([N1,N2,N3,N4,N5]).
+
+optionForNext5([_,L2,L3,L4,L5], Nexts, [L2,L3,L4,L5,P]) :-
+    contains(P,Nexts).
+
+recNextOptions(_,[],0).
+recNextOptions([L1,L2,L3,L4,L5], [P|ResNexts], Depth) :-
+    Depth > 0,
+    nextPossible5([L1,L2,L3,L4,L5], Nexts),
+    contains(P,Nexts),
+    Depth1 is Depth-1,
+    recNextOptions([L2,L3,L4,L5,P], ResNexts, Depth1).
